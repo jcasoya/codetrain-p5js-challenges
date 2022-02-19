1c8ca148810238ab6a7929a74b622d8940265891
jcasoya <jcasoya@gmail.com>
2022-02-19 10:23:21 +0100

    modularity one

---
+++ Violet_block_2022_02_19_09_09_50/sketch.js
@@ -0,0 +1,28 @@
+let ball = {
+  x: 0,
+  y: 0,
+  xspeed: 8,
+  yspeed: -3,
+}
+
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw() {
+  background(0);
+  stroke(255);
+  strokeWeight(4);
+  noFill();
+  ellipse(ball.x, ball.y, 24, 24);
+  
+  if (ball.x > width || ball.x < 0) {
+    ball.xspeed = -ball.xspeed;  
+  }
+  if (ball.y > height || ball.y < 0) {
+      ball.yspeed = ball.yspeed * -1;
+  }
+  
+  ball.x = ball.x + ball.xspeed;
+  ball.y = ball.y  + ball.yspeed;
+}
