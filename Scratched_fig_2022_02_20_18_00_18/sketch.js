2088484d6298182bb651c4519ddcf5717a84bd6c
jcasoya <jcasoya@gmail.com>
2022-02-20 19:13:59 +0100

    with let and repeating code

--- Scratched_fig_2022_02_20_18_00_18/sketch.js
+++ Scratched_fig_2022_02_20_18_00_18/sketch.js
@@ -9,9 +9,17 @@ function draw() {
   strokeWeight(4);
   stroke(255);
   
+  let x = 0;
+
   ellipse(0, 200, 25, 25);
+  x = x + 50;
   ellipse(50, 200, 25, 25);
+  x = x + 50;
   ellipse(100, 200, 25, 25);
+  x = x + 50;
   ellipse(150, 200, 25, 25);
+  x = x + 50;
   ellipse(200, 200, 25, 25);
+  x = x + 50;
   ellipse(250, 200, 25, 25);
+  x = x + 50;
