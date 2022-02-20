805af43c2b8fee346410ddb66c2a5b81593ab991
jcasoya <jcasoya@gmail.com>
2022-02-20 19:24:46 +0100

    wirh shorthand

--- Scratched_fig_2022_02_20_18_00_18/sketch.js
+++ Scratched_fig_2022_02_20_18_00_18/sketch.js
@@ -9,15 +9,8 @@ function draw() {
   strokeWeight(4);
   stroke(255);
   
-  let x = 0;
-
-  while (x <= width) {
-    ellipse(x, 200, 25, 25);
-    x = x + 50;
-  }
-}
-  
-for ( let x = 0; x <= width; x = x +50) {
+for ( let x = 0; x <= width; x += 50) {
   fill(255, 0, 200);
   ellipse(x, 300, 25, 25);
+}
 }
