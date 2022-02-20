3c80b0083212ded43f9ced8a08b88978d81fcaad
jcasoya <jcasoya@gmail.com>
2022-02-20 19:04:41 +0100

    changes else if to boolean

--- Scratched_fig_2022_02_20_18_00_18/sketch.js
+++ Scratched_fig_2022_02_20_18_00_18/sketch.js
@@ -9,12 +9,17 @@ function draw() {
   strokeWeight(4);
   noFill();
   
-  if (mouseX > 250) {
-    ellipse(300, 200, 100, 100);
-  } else if (mouseX > 150) {
-    rect(300, 200, 100, 100);
-  } else if (mouseX > 50) {
-    line(0, 0, width, height);
-  } else {
-    point(300, 200);
+  // if (mouseX > 250) {
+  //   ellipse(300, 200, 100, 100);
+  // } else if (mouseX > 150) {
+  //   rect(300, 200, 100, 100);
+  // } else if (mouseX > 50) {
+  //   line(0, 0, width, height);
+  // } else {
+  //   point(300, 200);
+  // }
+
+  if (mouseX > 300 && mouseX < 400) {
+    fill(255, 0, 200);
   }
+  rect(300, 200, 100, 100);
