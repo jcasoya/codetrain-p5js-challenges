9cd4492c032e4772de54a516bf01098ee4da4ec2
jcasoya <jcasoya@gmail.com>
2022-02-21 10:51:02 +0100

    function basics ball bouncing with if statements

--- p5js_Function_Basics_2022_02_21_09_44_29/sketch.js
+++ p5js_Function_Basics_2022_02_21_09_44_29/sketch.js
@@ -17,4 +17,13 @@ function draw() {
   ellipse(ball.x, ball.y, 24, 24);
 }
 
-if (ball.x > width || ball.x <)
+if (ball.x > width || ball.x < 0) {
+  ball.xspeed = -ball.xspeed;
+}
+if (ball.y > height || ball.y < 0) {
+  ball.yspeed = -ball.yspeed;
+}
+
+ball.x = ball.x + ball.xspeed;
+ball.y = ball.y + ball.yspeed;
+}
