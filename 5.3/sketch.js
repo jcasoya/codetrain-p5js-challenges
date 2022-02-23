d8ca1c10bfc68afaab623f76598e7e60e10907de
jcasoya <jcasoya@gmail.com>
2022-02-23 10:54:35 +0100

    functions and return miles to km function

--- 5.3/sketch.js
+++ 5.3/sketch.js
@@ -1,8 +1,12 @@
 function setup {
-    let angle = degrees(PI/2);
-    println(angle);
+    let km = milesToKm(26.3);
+    console.log(angle);
+
+    let km2 = milesToKm(100);
+    console.log(km2)
 }
 
-function draw {
-    
+function milesToKm(miles) {
+    let km = miles * 1.6;
+    return km;
 }
