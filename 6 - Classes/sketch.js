964fd986376cad4cbca8764f9cb43461991842c8
jcasoya <jcasoya@gmail.com>
2022-02-24 11:18:31 +0100

    bubble class

--- 6 - Classes/sketch.js
+++ 6 - Classes/sketch.js
@@ -3,8 +3,16 @@ let bubble;
 function setup() {
     createCanvas(600, 400);
     bubble = new Bubble();
+    print(bubble.x, bubble.y);
 }
 
 function draw() {
     background(0);
+}
+
+class Bubble {
+    constructor() {
+        this.x = 200;
+        this.y = 150;
+    }
 }
