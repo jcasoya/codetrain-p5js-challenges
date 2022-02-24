4b24a692814c82c40a70e94b8c8b7e118ddba940
jcasoya <jcasoya@gmail.com>
2022-02-24 11:23:45 +0100

    bubble class with functions

--- 6 - Classes/sketch.js
+++ 6 - Classes/sketch.js
@@ -15,4 +15,16 @@ class Bubble {
         this.x = 200;
         this.y = 150;
     }
+
+    move() {
+        this.x = this.x + random(-5, 5);
+        this.y = this.y + random(-5, 5);
+    }
+
+    show() {
+        stroke(255);
+        strokeWeight(4);
+        noFill();
+        ellipse(this.x, this.y, 24, 24);
+    }
 }
