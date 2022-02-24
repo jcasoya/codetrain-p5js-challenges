8cd6446f4d785b487cde080c7c3a05817f7ce0f5
jcasoya <jcasoya@gmail.com>
2022-02-24 11:31:14 +0100

    creating two bubble instantces

--- 6 - Classes/sketch.js
+++ 6 - Classes/sketch.js
@@ -1,13 +1,18 @@
-let bubble;
+let bubble1;
+let bubble2;
 
 function setup() {
     createCanvas(600, 400);
-    bubble = new Bubble();
-    print(bubble.x, bubble.y);
+    bubble1 = new Bubble();
+    bubble2 = new Bubble();
 }
 
 function draw() {
     background(0);
+    bubble1.move();
+    bubble1.show();
+    bubble2.move();
+    bubble2.show();
 }
 
 class Bubble {
